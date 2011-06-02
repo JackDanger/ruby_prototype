47f26e6c1ea28f774b43745ab7af055878594431
David Young-Chan Kay <davidykay@gmail.com>
2011-06-01 20:32:46 -0400

    Added silly comments

--- lib/ruby_prototype.rb
+++ lib/ruby_prototype.rb
@@ -1,11 +1,19 @@
 module RubyPrototype
   def method_missing(method_name, *arguments, &block)
+    # Default to standard behaviour, UNLESS:
+    # A. We receive an equals sign and one more parameter.
+    # B. We received a block.
     return super unless method_name.to_s =~ /=$/ &&
                         ((1 == arguments.length) | block_given?)
 
+    # If arguments is a proc and not a block, the proc is our block.
     block = arguments[0] if arguments[0].is_a?(Proc) && !block_given?
+    # Otherwise, take the arguments and expand them into a list.
+    # Q: Why do we do this? Are they then evaluated individually?
     block ||= proc {|*a| arguments[0] }
-    
+
+    # Define the missing method on the target class.
+    # We pass the method name, stripping the '=' from any assignments.
     (class << self; self; end).send :define_method,
                                     method_name.to_s.chomp('='),
                                     &block
@@ -13,7 +21,9 @@ module RubyPrototype
 end
 
 if RUBY_VERSION.to_f < 1.9
+  # Doesn't work on anything less than Ruby 1.9.
   warn "RUBY_VERSION 1.9+ required for RubyPrototype - skipping"
 else
+  # Send the message, including this module.
   Object.send :include, RubyPrototype
 end
--- lightning.rb
+++ lightning.rb
@@ -2,13 +2,16 @@ require 'ruby_prototype'
 
 User = Object.new
 User.name = 'Samson'
+# The 'name' property springs into existence on the User object.
 
+# We can also pass procs!
 User.extends = proc do |hash|
   hash.each do |property, value|
     send "#{property}=", value
   end
 end
 
+# Here's an example of passing a hash to the proc we just created.
 User.extends :name => 'Delilah'
 
 puts User.name # => 'Delilah'
